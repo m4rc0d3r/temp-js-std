b33255902576f21dfc8518fa1bc8f08ec22daed2
Mark <m4rc0d3r@gmail.com>
2025-10-26 01:47:58 +0300

    feat: create functions for check if value is truthy or falsy

---
+++ src/bool.ts
@@ -0,0 +1,23 @@
+import { EMPTY } from "./consts";
+
+type Falsy = false | 0 | 0n | "" | null | undefined;
+type Truthy<T> = Exclude<T, Falsy>;
+
+function isFalsy(value: unknown): value is Falsy {
+  return (
+    value === false ||
+    value === EMPTY ||
+    value === 0 ||
+    value === 0n ||
+    (typeof value === "number" && isNaN(value)) ||
+    value === null ||
+    value === undefined
+  );
+}
+
+function isTruthy<T>(value: T): value is Truthy<T> {
+  return !isFalsy(value);
+}
+
+export { isFalsy, isTruthy };
+export type { Falsy, Truthy };
