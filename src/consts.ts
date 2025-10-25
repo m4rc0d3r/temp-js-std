14431aa98afde5bd54c2d7112dfc07921846fec3
Mark <m4rc0d3r@gmail.com>
2025-10-26 01:25:04 +0300

    feat: add str constant for backslash symbol

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,6 @@ const EMPTY = "";
 const SPACE = " ";
 const NEW_LINE = "\n";
 const SLASH = "/";
+const BACKSLASH = "\\";
 
-export { EMPTY, NEW_LINE, SLASH, SPACE };
+export { BACKSLASH, EMPTY, NEW_LINE, SLASH, SPACE };
