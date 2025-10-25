34a3c60e0db31209002404d34a5ce1894a65dbe2
Mark <m4rc0d3r@gmail.com>
2025-10-26 01:36:31 +0300

    fix: leave untouch all chars except first in 'capitalize' function

--- src/case.ts
+++ src/case.ts
@@ -2,7 +2,6 @@ import { SPACE } from "./consts";
 
 const toLower = <T extends string>(value: T) => value.toLowerCase() as Lowercase<T>;
 const toUpper = <T extends string>(value: T) => value.toUpperCase() as Uppercase<T>;
-const capitalize = (value: string) =>
-  [value[0]?.toUpperCase(), value.slice(1).toLowerCase()].join(SPACE);
+const capitalize = (value: string) => [value[0]?.toUpperCase(), value].join(SPACE);
 
 export { capitalize, toLower, toUpper };
