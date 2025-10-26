38372a4dd7549e9f6fade1cdc3902ec7b2a1d782
Mark <m4rc0d3r@gmail.com>
2025-10-26 23:07:58 +0200

    feat: create 'isInt' predicate

--- src/num.ts
+++ src/num.ts
@@ -1,5 +1,6 @@
 const isEven = (value: number) => value % 2 === 0;
 const isOdd = (value: number) => !isEven(value);
 const toInt = (value: number) => Math.floor(value);
+const isInt = (value: number) => Number.isInteger(value);
 
-export { isEven, isOdd, toInt };
+export { isEven, isInt, isOdd, toInt };
