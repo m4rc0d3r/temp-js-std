345360d9c7c2fbfb30e163482c2d76bc0b8df8df
Mark <m4rc0d3r@gmail.com>
2026-04-03 00:12:17 +0300

    feat: create 'toJson' function

--- src/num.ts
+++ src/num.ts
@@ -1,10 +1,16 @@
+import type { JsonValue } from "type-fest";
+
 const isEven = (value: number) => value % 2 === 0;
 const isOdd = (value: number) => !isEven(value);
 const toInt = (value: number) => Math.floor(value);
 const isInt = (value: number) => Number.isInteger(value);
 
-export default function parseJson(value: string) {
+function parseJson(value: string) {
   return JSON.parse(value) as unknown;
 }
 
-export { isEven, isInt, isOdd, toInt };
+function toJson(value: JsonValue) {
+  JSON.stringify(value);
+}
+
+export { isEven, isInt, isOdd, parseJson, toInt, toJson };
